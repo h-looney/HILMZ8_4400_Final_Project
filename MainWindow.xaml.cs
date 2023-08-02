364db00947b27d80d711ba269e7631796ead88bb
Holden Looney <88345405+h-looney@users.noreply.github.com>
2023-08-02 15:36:17 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using SchoolKeep.ViewModels;
+
+namespace SchoolKeep
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+            ((MainWindowViewModel)DataContext).FilePath = @"C:\Users\holde\OneDrive\Documents\INFOTC4400";
+            ((MainWindowViewModel)DataContext).HomeworkList = new ObservableCollection<HomeworkAssignmentViewModel>();
+            ((MainWindowViewModel)DataContext).SelectedFolder = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            FileSystemInfoViewModel item = (FileSystemInfoViewModel)e.NewValue;
+            if (item !=  null)
+            {
+                var data = ((TreeView)e.OriginalSource).DataContext;
+                ((MainWindowViewModel)data).SelectedFolder = item.ToString();
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
